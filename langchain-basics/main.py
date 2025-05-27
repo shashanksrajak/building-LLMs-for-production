e0f35d2ced95172e35968543b3187a540163beec
Shashank Rajak <shashanksrajak@gmail.com>
2025-05-27 17:12:55 +0530

    use uv for package manager

---
+++ langchain-basics/main.py
@@ -0,0 +1,23 @@
+from langchain.chat_models import init_chat_model
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if not os.environ.get("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass(
+        "Enter API key for Google Gemini: ")
+
+
+def main():
+    print("Hello from building-llms-for-production!")
+
+    model = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+
+    response = model.invoke("Hello Gemini! What do you think about LangChain?")
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
